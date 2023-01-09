e75248b8c300bd28f2d2f66ddc5fd78a7bda0eea
fxsbl <117072635+fxsbl@users.noreply.github.com>
2023-01-09 10:57:50 +0530

    Add files via upload

---
+++ New folder/firstProgram.py
@@ -0,0 +1,23 @@
+# first = input("Enter Your Number: ")
+# operator = input("Inpurt Your Operator (+,-,/,*,%): ")
+# second = input("Enter Your Second Number: ")
+
+# first = int(first)
+# second = int(second)
+
+# if operator == "+":
+#     print(first + second)
+# elif operator == "-":
+#     print(first - second)
+# elif operator == "*":
+#     print(first * second)
+# elif operator == "/":
+#     print(first / second)
+# elif operator == "%":
+#     print(first % second)
+# else:
+#     print("Invalid Operation")
+
+
+no = range (5)
+print(no)
